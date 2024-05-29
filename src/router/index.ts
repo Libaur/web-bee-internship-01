21f66e1d568253c204bab66666bb716760a11d49
Alexander Libaur <113611530+Libaur@users.noreply.github.com>
2024-05-29 15:49:06 +0300

    Revert "final refactor"

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,44 +8,42 @@ import { renderMap } from "../map";
 
 const routes: Routes = {
   paths: {
-    "/web-bee-internship-01/": "profile.html",
-    "/web-bee-internship-01/map": "map.html",
-    "/web-bee-internship-01/time": "timer.html",
+    "/web-bee-internship-01/404": "/public/404.html",
+    "/web-bee-internship-01/": "/public/profile.html",
+    "/web-bee-internship-01/map": "/public/map.html",
+    "/web-bee-internship-01/time": "/public/timer.html",
   },
   titles: {
+    "/web-bee-internship-01/404": "Page not found",
     "/web-bee-internship-01/": "Profile",
     "/web-bee-internship-01/map": "Map",
     "/web-bee-internship-01/time": "Time",
   },
 };
 
-document.addEventListener("DOMContentLoaded", redirectToProfile);
+document.addEventListener("DOMContentLoaded", function () {
+  window.history.replaceState({}, "", "/web-bee-internship-01/");
+  handleLocation();
+});
 
-function redirectToProfile() {
-  if (!window.location.pathname.startsWith("/web-bee-internship-01/")) {
-    window.history.replaceState({}, "", "/web-bee-internship-01/");
-    handleLocation();
-  }
-}
-
-async function handleLocation() {
+const handleLocation = async () => {
   const path = window.location.pathname;
-  const route = routes.paths[path];
-  const title = routes.titles[path];
+  const route = routes.paths[path] || routes.paths["404"];
+  const title = routes.titles[path] || routes.titles["404"];
   const html = await fetch(route).then((data) => data.text());
   setupPage(html, title);
   renderMap(path);
   restoreActiveLink();
-}
+};
 
-function navigateTo(event: Event) {
+const navigateTo = (event: Event) => {
   event.preventDefault();
   const target = event.target as HTMLAnchorElement;
   window.history.pushState({}, "", target.href);
   updateNavBarClassList(target);
   updateStoredActiveLink(target);
   handleLocation();
-}
+};
 
 document.querySelectorAll(".nav-link").forEach((link) => {
   link.addEventListener("click", navigateTo);
